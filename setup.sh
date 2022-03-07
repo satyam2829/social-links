243b40aa5d651b38e9b5daa47906ca59046584db
satyam2829 <satyamanand5@gmail.com>
2022-03-07 16:21:29 +0530

    modify set.sh file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/.streamlit/
-<<<<<<< HEAD
+
 echo "
 [general]n
 email = "satyamanand5@gmail.com"n
@@ -10,7 +10,7 @@ headless = truen
 enableCORS=falsen
 port = $PORTn
 " > ~/.streamlit/config.toml
-=======
+
 echo "\
 [general]\n\
 email = \"satyamanand5@gmail.com\"\n\
@@ -21,4 +21,4 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
->>>>>>> 7da22443822e613282a3720991081f54e9ba6de5
+
